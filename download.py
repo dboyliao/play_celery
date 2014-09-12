8c134637de62c978ec5e540430927f4ebd7256ec
dboyliao <qmalliao@gmail.com>
2014-09-12 22:18:07 +0800

    testing

--- download.py
+++ download.py
@@ -1,5 +1,5 @@
 from scp_celery.tasks import download
-from local_settings import num_of_computer, TARGET_URL
+from local_settings import num_of_computer
 
 for i in range(num_of_computer):
-	download.delay(TARGET_URL)
+	download.delay("http://140.116.21.177/NCKUMath201409.ova")
--- scp_celery/local_settings.py
+++
@@ -1,4 +0,0 @@
-BROCKER_URL = 'mongodb://140.116.21.177:27017/helloCeleryBroker'
-BACKEND_URL =  'mongodb://140.116.21.177:27017/helloCeleryBackend'
-num_of_computer = 7
-TARGET_URL = "http://140.116.21.177/NCKUMath201409.ova"
